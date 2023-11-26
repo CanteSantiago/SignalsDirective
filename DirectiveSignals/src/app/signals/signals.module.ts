a10593c05b40b938488f4ba8366f37d5254a6c0a
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-25 22:45:26 -0500

    Vid325-Menu lateral

--- DirectiveSignals/src/app/signals/signals.module.ts
+++ DirectiveSignals/src/app/signals/signals.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SignalsRoutingModule } from './signals-routing.module';
 import { SignalsLayoutComponent } from './layout/signalsLayout/signalsLayout.component';
+import { SideMenuComponent } from './components/sideMenu/sideMenu.component';
+import { CounterPageComponent } from './pages/counterPage/counterPage.component';
+import { UserInfoPageComponent } from './pages/userInfoPage/userInfoPage.component';
+import { PropertiesPageComponent } from './pages/propertiesPage/propertiesPage.component';
 
 
 @NgModule({
   declarations: [
     SignalsLayoutComponent,
-
-
+    CounterPageComponent,
+    UserInfoPageComponent,
+    PropertiesPageComponent,
+    SideMenuComponent
   ],
   imports: [
     CommonModule,
