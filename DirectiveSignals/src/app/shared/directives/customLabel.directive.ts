cc55be7576b473080909f3ef6b6f67d06a307d76
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-25 21:26:42 -0500

    Vid319-Cambiar el mensaje de la etiqueta

--- DirectiveSignals/src/app/shared/directives/customLabel.directive.ts
+++ DirectiveSignals/src/app/shared/directives/customLabel.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
@@ -8,10 +9,17 @@ export class CustomLabelDirective implements OnInit{
 
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
+
 
   @Input() set color( value: string ) {
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors( value: ValidationErrors | null | undefined ) {
+    this._errors = value;
+    this.setErrorMessage();
   }
 
   constructor( private el: ElementRef<HTMLElement> ) {
@@ -31,5 +39,21 @@ export class CustomLabelDirective implements OnInit{
     this.htmlElement!.nativeElement.style.color = this._color;
   }
 
+  setErrorMessage():void {
+    if ( !this.htmlElement )return;
+    if ( !this._errors ) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+    const errors = Object.keys(this._errors);
+    console.log(errors)
+    if ( errors.includes('required') )  {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido.';
+      return;
+    }
+
+
+
+  }
 
 }
