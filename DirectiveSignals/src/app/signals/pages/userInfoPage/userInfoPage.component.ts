7dfc4d107210027133c9d069351758579f18c72e
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-26 00:46:44 -0500

    Vid329-Señales-Informacion del usuario

--- DirectiveSignals/src/app/signals/pages/userInfoPage/userInfoPage.component.ts
+++ DirectiveSignals/src/app/signals/pages/userInfoPage/userInfoPage.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UserServiceService } from '../../services/user-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-userInfoPage',
   templateUrl: './userInfoPage.component.html',
   styleUrls: ['./userInfoPage.component.css']
 })
-export class UserInfoPageComponent implements OnInit {
+export class UserInfoPageComponent  implements OnInit {
 
-  constructor() { }
+  private usersService = inject(UserServiceService);
+  public userId = signal(1);
 
-  ngOnInit() {
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser( this.userId() )
+  }
+
+  loadUser( id: number ) {
+    if ( id <= 0 ) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById( id )
+      .subscribe({
+        next: (user) => {
+         this.currentUser.set( user );
+
+        }
+      });
+
+
   }
 
 }
