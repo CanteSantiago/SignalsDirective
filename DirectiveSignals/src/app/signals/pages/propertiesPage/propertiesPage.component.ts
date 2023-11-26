0a052bc0f6da44f057e96b23d73c54c649bb0ad1
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-26 01:17:17 -0500

    Vid332-Mutaciones
    Actualizar parte del estado

--- DirectiveSignals/src/app/signals/pages/propertiesPage/propertiesPage.component.ts
+++ DirectiveSignals/src/app/signals/pages/propertiesPage/propertiesPage.component.ts
@@ -1,17 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-propertiesPage',
   templateUrl: './propertiesPage.component.html',
   styleUrls: ['./propertiesPage.component.css']
 })
-export class PropertiesPageComponent implements OnInit {
+export class PropertiesPageComponent  {
 
-  onFieldUpdated(field: string,value:string){
-    console.log({field,value})
+
+  public user = signal<User>( {
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg'
+  } );
+
+
+  onFieldUpdated( field: keyof User, value: string ) {
+
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value,
+    // });
+
+    // this.user.update( current => ({
+    //   ...current,
+    //   [field]: value
+    // }))
+
+    this.user.update( current => {
+
+      switch ( field ) {
+
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+        case 'id':
+          current.id = Number( value );
+        break;
+      }
+
+
+      return current;
+    } );
+
+
   }
-
-  ngOnInit() {
-  }
-
 }
